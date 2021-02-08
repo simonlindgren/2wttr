ab269c93b0a8caebc1b30f5dce777fe7b5c1a73e
Simon Lindgren <simon.lindgren@umu.se>
2021-02-08 19:27:34 +0100

    update config_fas.py

--- config_fas.py
+++ config_fas.py
@@ -3,9 +3,6 @@ bearer_token = "your-bearer-token-here"
 # Query (Example: '(from:twitterdev -is:retweet) OR #twitterdev')
 query = 'query-entered-here'
 
-tweet_fields = 'created_at,author_id,lang'
-user_fields = 'id,name,username,location'
-
 start_time = "2018-01-01T00:00:00Z"
 end_time = "2018-01-01T00:19:00Z"
 
